b0f61d5dafb5f4446283db6616774d20df98ced6
anshu <anshu.ar97@gmail.com>
2026-02-07 01:00:46 +0530

    4rth

--- src/test/java/com/qa/otherTestCase/FindAllLink.java
+++ src/test/java/com/qa/otherTestCase/FindAllLink.java
@@ -14,35 +14,34 @@ import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class FindAllLink {
-	
-WebDriver driver;
-	
+
+	WebDriver driver;
+
 	@BeforeMethod
-	public void Launch2	() {
+	public void Launch2() {
 
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		driver.get("https://club.ministryoftesting.com/");
-		
+		driver.get("https://club.ministryoftesting.com");
+
 	}
+
 	@Test
 	public void findAllLink_TestCase() {
 		java.util.List<WebElement> linklist1 = driver.findElements(By.tagName("a"));
-		System.out.println("Total Link is :" + " "+ linklist1.size());
-		
+		System.out.println("Total Link is :" + " " + linklist1.size());
+
 		for (WebElement e : linklist1) {
-			
+
 			String alllinkPrint = e.getText();
 			System.out.println(alllinkPrint);
-			
+
 		}
-		
-		
+
 	}
-	
-	
+
 	@AfterMethod
 	public void teardown() {
 		driver.close();
